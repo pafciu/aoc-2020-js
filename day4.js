18f284f897f4ef40c68389e47a10a30855bcb517
pafciu <paafec+github@gmail.com>
2021-02-19 20:57:28 +0100

    part 1

--- day4.js
+++ day4.js
@@ -1,22 +1,49 @@
-fetch('day4-example.txt').then(function(response) {
+const required_fields = new Set(['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']);
 
-    response.text().then(function(text) {
+function contains_required(fields) {
+    
+    for (let rf of required_fields) {
+        
+        if (!fields.has(rf)) {
 
-        const lines = text.split('\n');
-        const required_fields = new Set(['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']);
-
-        for (let i = 0; i < lines.length; i++) {
-
-            if (lines[i].length !== 0) {
-
-                const kvs = lines[i].split(' ');
-
-                for (let j = 0; j < kvs.length; j++) {
-
-                    console.log(kvs[j].slice(0, 3));
-                    console.log(kvs[j].slice(4, kvs[j].length));
-                }
-            }
+            return false;
         }
-    });
+    }
+    return true;
+}
+
+function valid_passports(text) {
+
+    const lines = text.split('\n');
+    let fields = new Set();
+    let valid = 0;
+
+    for (let i = 0; i < lines.length; i++) {
+
+        if (lines[i].length !== 0) {
+
+            const kvs = lines[i].split(' ');
+
+            for (let j = 0; j < kvs.length; j++) {
+
+                fields.add(kvs[j].slice(0, 3));
+            }
+
+        } else {
+
+            if (contains_required(fields)) {
+
+                valid++;
+            }
+
+            fields = new Set();
+        }
+    }
+
+    document.querySelector('p').innerText = valid.toString();
+}
+
+fetch('day4.txt').then(function(response) {
+
+    response.text().then(valid_passports);
 });
