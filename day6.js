416b0e379afec3d1bd7c772848df41847f8eb4f6
pafciu <paafec+github@gmail.com>
2021-02-23 10:56:30 +0100

    part 2

--- day6.js
+++ day6.js
@@ -1,23 +1,35 @@
+function intersection(setA, setB) {
+    let _intersection = new Set()
+    for (let elem of setB) {
+        if (setA.has(elem)) {
+            _intersection.add(elem)
+        }
+    }
+    return _intersection
+}
+
 fetch('day6.txt').then(function(response) {
 
     response.text().then(function(text) {
 
-        let unique_answers = new Set();
+        let everyone_answers = new Set();
         let count = 0;
 
         for (let line of text.split('\n')) {
 
             if (line.length !== 0) {
                 
+                const answers = new Set();
                 for (let c of line) {
 
-                    unique_answers.add(c);
+                    answers.add(c);
                 }
+                everyone_answers.add(answers);
 
             } else {
 
-                count += unique_answers.size;
-                unique_answers = new Set();
+                count += [...everyone_answers].reduce((a, c) => intersection(a, c)).size;
+                everyone_answers = new Set();
             }
         }
 
